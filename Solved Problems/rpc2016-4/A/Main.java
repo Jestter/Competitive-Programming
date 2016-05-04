390f75a63a38f9664b838d2744b30646228ffb0a
Jestter <fabian.07.03.92@gmail.com>
2016-05-03 22:19:00 -0300

    rpc16-4 problem A solution

---
+++ Solved Problems/rpc2016-4/A/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+	
+	public static void main(String[] args) 
+	{
+		
+		new Main();
+	}
+        
+       
+	public Main() 
+    {
+            Scanner sc = new Scanner(System.in);
+            while(sc.hasNext())
+            {
+            	String s= sc.nextLine();
+            	if(s.length()==1&&s.charAt(0)=='0')break;
+            	BigInteger bi = new BigInteger(s);
+            	String res = bi.multiply(bi.add(new BigInteger("1"))).divide(new BigInteger("2")).pow(2).toString();
+            	if(res.length()>10)res = res.substring(res.length()-10,res.length());
+            	System.out.println(new BigInteger(res));
+            }
+	}
+
+}
---
+++ Solved Problems/rpc2016-4/A/comexecjav.bat
@@ -0,0 +1,4 @@
+javac Main.java
+
+java Main < test.in > test.out
+PAUSE
