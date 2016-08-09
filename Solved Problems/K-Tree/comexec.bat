0935cef719a14cd3bc34ddd5810dca10958ce1e1
Jestter <fabian.07.03.92@gmail.com>
2016-08-09 17:26:03 -0400

    k tree ac

--- Solved Problems/K-Tree/comexec.bat
+++
@@ -1,5 +0,0 @@
-g++ main.cpp -std=c++11 -o main.exe
-main.exe < test.in > test.out
-start test.out
-
-PAUSE
--- Solved Problems/K-Tree/main.cpp
+++ Solved Problems/K-Tree/main.cpp
@@ -7,26 +7,28 @@ int main()
 	int n,k,d;
 	cin >> n >> k >> d;
 	long long dp[101][101];
-	for (int i = 0; i < 100; ++i)
+	for (int i = 0; i <= 100; ++i)
 	{
-		for (int j = 0; j < 100; ++j)
+		for (int j = 0; j <= 100; ++j)
 		{
-			if(i==j)dp[i][j] = 1;
-			else dp[i][j] = 0;
+			dp[i][j] = 0;
 		}
 	}
-
+	dp[0][0]=0;
+	dp[1][1]=1;
 	for (int i = 2; i <= n; ++i) //para n
 	{
 		for (int j = 1; j <= k && j<=i; ++j) //para d
 		{
-			cout << i << " " << j << endl;
-			for (int l = 1; l <= k && l<j; ++l) // para cada arista desde origen n d
+			//cout << i << " " << j << " = ";
+			for (int l = 1; l <= k && l<=i; ++l) // para cada arista desde origen n d
 			{
-				cout << l << endl;
-				dp[i][j] += dp[i-l][(l<j?j:1)];
+				//cout << (l==1?"":" + ") << dp[i-l][(l<j?j:1)];
+				if(i-l==0) dp[i][j]++;
+				else dp[i][j] += dp[i-l][(l<j?j:1)];
+				dp[i][j]%=1000000007;
 			}
-			cout << " = " << dp[i][j] << endl;
+			//cout << " = " << dp[i][j] << endl;
 		}
 	}
 
