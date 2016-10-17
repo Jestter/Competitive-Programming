2ea165df9215f555015f89c2153e16b53d101243
Jestter <fabian.07.03.92@gmail.com>
2016-10-17 18:36:10 -0300

    stackem up

---
+++ Solved Problems/stackem_up/comexec.sh
@@ -0,0 +1,3 @@
+g++ main.cpp -std=c++11 -o main
+./main < test.in > test.out
+subl test.out
---
+++ Solved Problems/stackem_up/main.cpp
@@ -0,0 +1,95 @@
+//UVA 10205
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string value(int a)
+{
+	int b = a%13;
+
+	string s = "";
+
+	switch(b)
+	{
+		case 12:
+			s = "Ace"; break;
+		case 9:
+			s = "Jack"; break;
+		case 10:
+			s = "Queen"; break;
+		case 11:
+			s = "King"; break;
+		default:
+			s = to_string(b+2);
+	}	
+	return s;
+}
+
+string suit(int a)
+{
+	int b = a/13;
+	switch(b)
+	{
+		case 0: return "Clubs";
+		case 1: return "Diamonds";
+		case 2: return "Hearts";
+		case 3: return "Spades";
+		default: break;
+	}
+	return "none";
+}
+
+
+int main()
+{
+	int tc;
+	scanf("%d",&tc);
+	while(tc--)
+	{
+		int nshuffles;
+		scanf("%d",&nshuffles);
+		int shuffles[nshuffles][52];
+		
+		for (int i = 0; i < nshuffles; ++i)
+		{
+			for (int j = 0; j < 52; ++j)
+			{
+				int a;
+				scanf("%d",&a);
+				shuffles[i][a-1] = j;
+			}
+		}
+		int deck[2][52];
+		for (int i = 0; i < 52; ++i)
+		{
+			deck[0][i] = i;
+			deck[1][i] = i;
+		}
+
+		int usin = 0;
+		char sh;
+		while(scanf("%c",&sh) != EOF)
+		{
+			int ant = (int)sh;
+			scanf("%c",&sh);
+			if(ant == (int)sh)break;
+			//cout << ((int)sh-'0') << endl;
+
+			int usin2 = (usin+1)%2;
+			for (int i = 0; i < 52; ++i)
+			{
+				deck[usin2][shuffles[((int)sh-'0')-1][i]] = deck[usin][i];
+
+				//cout << "deck["<<(shuffles[((int)sh-'0')-1][i])<< "] = " << deck[usin][i] << endl;
+			}
+
+			usin = usin2; 
+		}
+
+		for (int i = 0; i < 52; ++i)
+		{
+			cout << value(deck[usin][i]) << " of " << suit(deck[usin][i]) << endl;
+		}
+		if(tc!=0)cout << endl;
+	}
+} 
